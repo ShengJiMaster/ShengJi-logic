19cb5769d55cb0cf73ab28ca80226a5a68cd2d8b
williazz <williazz@usc.edu>
2020-06-24 01:11:32 -0700

    leadSuit and TrickWinner

--- src/components/TrickGame.js
+++ src/components/TrickGame.js
@@ -4,10 +4,12 @@ class TrickGame extends Game {
   constructor(minPlayers = 1, maxPlayers = minPlayers, options = {}) {
     super(minPlayers, maxPlayers, options);
     this.whoStartsRound = null;
+    this.trumpSuit = null;
+    this.trumpRank = null;
   }
 
   /** Cards are only played to the table when they becme apart of a trick
-   * @returns {Object} – The table
+   * @returns {Array}
    */
   get trick() {
     return this.table;
@@ -25,7 +27,7 @@ class TrickGame extends Game {
   }
 
   /**
-   * Invokes playCardToTable ONLY IF it's that player's turn to play cards to the table
+   * Invokes playCardOrGroupToTable ONLY IF it's that player's turn
    * @param {Number} playerIndex
    * @param {Number | [Number]} cardIndeces
    */
@@ -35,7 +37,33 @@ class TrickGame extends Game {
     return this.playCardOrGroupToTable(playerIndex, cardIndeces);
   }
 
-  get trickWinner() {}
+  /**
+   * @return {Number} – the suit index of the card played by the player who started the round
+   */
+  get leadSuit() {
+    const { trick, whoStartsRound } = this;
+    if (!trick.length) return;
+    return trick[whoStartsRound].s;
+  }
+
+  /**
+   * @returns {Number} – playerIndex of winner
+   */
+  get trickWinner() {
+    const { whosTurn, trick, leadSuit, trumpSuit, trumpRank } = this;
+    if (whosTurn !== null) return;
+    let winnerIndex;
+    let maxVal = -1;
+    for (let i = 0; i < trick.length; i++) {
+      const card = trick[i];
+      const val = card.toTrickRank(leadSuit, trumpSuit, trumpRank);
+      if (val > maxVal) {
+        maxVal = val;
+        winnerIndex = i;
+      }
+    }
+    return winnerIndex;
+  }
 }
 
 module.exports = TrickGame;
--- src/components/TrickGame.test.js
+++ src/components/TrickGame.test.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const TrickGame = require('./TrickGame');
+const Card = require('./Card');
 
 describe('TrickGame', () => {
   describe('init', () => {
@@ -29,6 +30,44 @@ describe('TrickGame', () => {
 
     it('should return null when everyone has already played', (done) => {
       expect(myGame.whosTurn).toBe(null);
+      done();
+    });
+  });
+
+  describe('leadSuit', () => {
+    const myGame = new TrickGame(4);
+    myGame.whoStartsRound = 2;
+    it('should return undefined if the table is empty', (done) => {
+      expect(myGame.leadSuit).toBe(undefined);
+      done();
+    });
+
+    it('should recognize the lead suit', (done) => {
+      myGame.table = _.range(4);
+      myGame.table[2] = new Card(7);
+      expect(myGame.leadSuit).toEqual(0);
+      done();
+    });
+  });
+
+  describe('trickWinner', () => {
+    const myGame = new TrickGame(4);
+    const whoStartsRound = 2;
+    myGame.whoStartsRound = whoStartsRound;
+    myGame.trumpRank = 5;
+    myGame.trumpSuit = 2;
+    for (let i = 0; i < 4; i++) myGame.addPlayer();
+    myGame.table = _.range(4).map((n) => new Card(n));
+    myGame.table[0] = new Card(36);
+
+    const checkWinner = () => myGame.trickWinner;
+
+    it('should identify a trump', (done) => {
+      let ans;
+      expect(() => {
+        ans = checkWinner();
+      }).not.toThrow(Error);
+      expect(ans).toEqual(0);
       done();
     });
   });
